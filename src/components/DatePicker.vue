b274e3e8458393e2a5e14404a778ffe7382c125e
NicholasZhurauliou <nicholaszhurauliou@gmail.com>
2021-01-27 14:20:42 +0300

    Create DatePicker component

---
+++ src/components/DatePicker.vue
@@ -0,0 +1,38 @@
+<template>
+  <DatePicker 
+    v-model="day" 
+    class="datepicker"
+    value-type="format" 
+    format="DD" 
+    inline
+    open
+  />
+</template>
+
+<script>
+  import DatePicker from "vue2-datepicker";
+  import "vue2-datepicker/index.css";
+  import "vue2-datepicker/locale/ru";
+
+export default {
+  components: { DatePicker },
+  props: {},
+  data () {
+    return { day: null };
+  }
+};
+</script>
+
+<style lang="scss">
+  .datepicker {
+    .mx-calendar-header {
+      display: none;
+    }
+    .not-current-month {
+      pointer-events: none;
+      div {
+        display: none;
+      }
+    }
+  }
+</style>
