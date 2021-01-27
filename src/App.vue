212093aaa502dc54dc631f95217b89403c4761e2
NicholasZhurauliou <nicholaszhurauliou@gmail.com>
2021-01-27 16:39:02 +0300

    Add select 'type work'

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,16 @@
     <hr/>
     <template v-if="isDay">
       <p>{{day}}e</p>
+      <select v-model="typeWork">
+        <option 
+          v-for="type in typeWorks"
+          :disabled="type.value === ''"
+          :value="type.value"
+          :key="type.value"
+        >
+          {{type.text}}
+        </option>
+      </select>
       <button @click="saveChanges">Сохранить</button>
     </template>
     <p v-else>Выберите день в календаре</p>
@@ -19,14 +29,22 @@
       DatePicker
     },
     data () {
-      return { day: null};
+      return { 
+        day: null,
+        typeWork: '',
+        typeWorks: [
+          { text: 'Вид работы', value: '' },
+          { text: 'Важная', value: 'important' },
+          { text: 'Обычная', value: 'routine' },
+        ]
+      };
     },
     methods: {
       changeDay (newDay) {
         this.day = newDay;
       },
       saveChanges () {
-        
+
       }
     },
     computed: {
@@ -38,4 +56,7 @@
 </script>
 
 <style lang="scss">
+  button {
+    margin-left: 10px;
+  }
 </style>
