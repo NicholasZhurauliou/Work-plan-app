44ebf823c2be74c450377389541999936447f813
NicholasZhurauliou <nicholaszhurauliou@gmail.com>
2021-02-03 15:01:32 +0300

    Add router in project

--- src/App.vue
+++ src/App.vue
@@ -1,94 +1,32 @@
 <template>
-  <div id="app" class="app">
-    <div class="app__container">
-      <day-picker :current-day="day" @change-current-day="changeDay"></day-picker>
-      <day-time :times="times"></day-time>
+  <div id="app">
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
     </div>
-    <hr/>
-    <div v-if="isDay">
-      <p>{{ day }}e</p>
-      <day-form @add-period="saveChanges"></day-form>
-    </div>
-    <p v-else>Выберите день в календаре</p>
+    <router-view />
   </div>
 </template>
 
-<script>
-  import DayPicker from './components/DayPicker.vue'
-  import DayTime from './components/DayTime.vue'
-  import DayForm from './components/DayForm.vue'
-  import {HOURS_IN_DAY} from './constants.js'
-  import {getLastDayInCurrentMonth} from './helpers.js'
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
 
-  export default {
-    name: 'App',
-    components: {
-      DayPicker,
-      DayTime,
-      DayForm
-    },
+#nav {
+  padding: 30px;
 
-    data() {
-      return { 
-        day: null,
-        days: this.initDays()
-      }
-    },
+  a {
+    font-weight: bold;
+    color: #2c3e50;
 
-    methods: {
-      initDays() {
-        let result = {}
-
-        for (let i = 1; i <= this.lastDay; i++) {
-          let periods = [...new Array(this.hours)].map(() => ({isImportant: false, isRoutine: false}))
-          result[i] = periods
-        }
-
-        return result
-      },
-
-      changeDay(newDay) {
-        this.day = newDay
-      },
-
-      saveChanges({start, end, type}) {
-        let isValid = !!(start && end && type)
-
-        if (isValid && (start < end)) {
-          this.days[this.day].forEach((period, idx) => {
-            let index = idx + 1
-            if ((index >= start) && (index < end)) {
-              let key = (type === 1) ? 'isImportant' : 'isRoutine'
-              period[key] = true
-            }
-          })
-        }
-      }
-    },
-
-    computed: {
-      isDay: function() {
-        return this.day !== null
-      },
-      times: function() {
-        if (this.day !== null) {
-          return this.days[this.day]
-        }
-        return []
-      } 
-    },
-
-     beforeCreate() {
-      this.hours = HOURS_IN_DAY
-      this.lastDay = getLastDayInCurrentMonth()
+    &.router-link-exact-active {
+      color: #42b983;
     }
   }
-</script>
-
-<style scoped lang="scss">
-  .app {
-    &__container {
-      display: flex;
-    }
-  }
+}
 </style>
--- src/main.js
+++ src/main.js
@@ -1,20 +1,22 @@
 import Vue from "vue";
 import App from "./App.vue";
 import store from "./store";
+import router from "./router";
 
-Vue.filter('format', function(value) {
-  let result = ''
+Vue.filter("format", function(value) {
+  let result = "";
 
-  if (value < 10) result += 0
-  
-  result += `${value}:00`
+  if (value < 10) result += 0;
 
-  return result
-})
+  result += `${value}:00`;
+
+  return result;
+});
 
 Vue.config.productionTip = false;
 
 new Vue({
   store,
+  router,
   render: h => h(App)
 }).$mount("#app");
