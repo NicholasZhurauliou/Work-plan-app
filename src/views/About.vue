142376535a95591915233fb912166616162fe008
NicholasZhurauliou <nicholaszhurauliou@gmail.com>
2021-02-05 09:31:09 +0300

    Add navigation guards

--- src/views/About.vue
+++ src/views/About.vue
@@ -17,6 +17,24 @@
       day() {
         return this.$store.state.day;
       }
+    },
+    watch: {
+      $route(to, from) {
+        const prevId = from.params.id;
+        const nextId = to.params.id;
+        if (prevId !== nextId) {
+          this.$store.commit("setDay", nextId);
+        }
+      }
+    },
+    beforeRouteEnter (to, from, next) {
+      next(vm => {
+        let id = +to.params.id;
+        let currentId = vm.day;
+        if (!isNaN(id) && id !== currentId) {
+          vm.$store.commit("setDay", id);
+        }
+      })
     }
   }
 </script>
